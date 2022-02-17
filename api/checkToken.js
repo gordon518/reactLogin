724e86b38eba3ad0f82c28b1ab289d6f0c3bf554
Gordon <gordon518168@yahoo.com>
2022-02-17 13:46:04 +0800

    Change authentication from session to JWT

---
+++ api/checkToken.js
@@ -0,0 +1,9 @@
+﻿const db=require('../sqlite/liteDb');
+const { checkToken } = require("../authorization");
+
+module.exports = {
+	post: (req, res) => {
+		res.json({err:null, userName: req.userName});
+	}
+};
+
