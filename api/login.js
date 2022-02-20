404a959e5e403560941a18340d5977be65b43bc3
Gordon <gordon518168@yahoo.com>
2022-02-20 14:17:32 +0800

    add encrypt and decrypt

--- api/login.js
+++ api/login.js
@@ -1,10 +1,20 @@
 ﻿const db=require('../sqlite/liteDb');
 const { generateToken } = require("../authorization");
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
 
 module.exports = {
 	post: (req, res) => {
 		const {userName, password} = req.body;
-		checkPass(userName, password, function(ret1) {
+		//decrypt password
+		const privateKey = fs.readFileSync(path.resolve(__dirname,'../key/private.key'));
+		const decryptedPassword = crypto.privateDecrypt({key:privateKey, padding:crypto.constants.RSA_PKCS1_PADDING}, Buffer.from(password,'base64')).toString();
+		//MD5 encrypt password
+		const md5Password = md5(decryptedPassword);
+		//check password
+		checkPass(userName, md5Password, function(ret1) {
 			if(!ret1.err) {
 				const token = generateToken({ userName: userName });
 				console.log(token);
@@ -30,7 +40,7 @@ function checkPass(userName, password, fn) {
 	db.query(sql,[userName, password],function(ret) {
 		if(!ret.err) {
 			if(ret.rows.length==1) {
-				fn(ret);
+				fn(ret);	
 			} else {
 				ret.err="Username or password error";
 				fn(ret);
@@ -46,3 +56,7 @@ function saveToken(userName, token, fn) {
 		fn(ret);
 	});
 }
+
+function md5 (text) {
+	return crypto.createHash('md5').update(text).digest('hex');
+};
