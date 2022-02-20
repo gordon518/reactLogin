b04751ff01df4c7a2a180e9810359e8294e662fb
Gordon <gordon518168@yahoo.com>
2022-02-20 14:09:21 +0800

    Encrypt password

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import {Input, Form, Button, notification} from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import JSEncrypt from 'jsencrypt';
 import cookie from 'js-cookie';
 import {get, post} from './fetch';
 import style from './css/Login.css';
 
+const publicKey = 
+`-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwNAiZQKSICH+psi9k1NR
+8x5+DqsnHfK/WAEqW6i2vLIbmcCwbJVx/rrf4g5z23qaZqV00PwMiDKCqgRNyGsY
+LeW6wdh9QeYUSoqM2LJKndmInQyUPt1zemPKo5Z9xDDDAguM//mi1jD1WWeVkCDN
+nKjviJkmYI+CU75QFjDl3b5yBKnuK15HeuKwROl80OZ5xaoH0NBGefdc5/ZsaWwa
+xfKCXCwj0PFC7UdtC2dHD0mU8C8pDmfK5hCY8E2RFm2CfI2qpda3JXYq7LbO6/nR
+u4U9Wv5vHqd07x495fHiKLsMTj1erzHEyqJGm9RwN2xuxfdK1nNkpP1X1bJd9Pen
+kwIDAQAB
+-----END PUBLIC KEY-----
+`
+
 export default (props) => {
     const handleLogin = (values) => {
-        var postData={'userName':values.userName, 'password':values.password};
+        //First Encrypt password
+        const encrypt = new JSEncrypt();
+        encrypt.setPublicKey(publicKey);
+        var postData={'userName':values.userName, 'password':encrypt.encrypt(values.password)};
+        //Then call URL to login
         props.setFetch(true);
         post('/login', postData).then((response)=> {
             var ret=JSON.parse(response);
